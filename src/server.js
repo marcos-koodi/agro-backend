34845382d92e01ea7f133676019fce6eb999e4c2
Irai_Sanchez <irai.roman@fatecitapetininga.edu.br>
2021-04-07 19:20:49 -0300

    versao nova

--- src/server.js
+++ src/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const route = require('./route');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+const allowedOrigins = [
+    'capacitor://localhost',
+    'ionic://localhost',
+    'http://localhost/',
+    'http://localhost:8080/',
+    'http://localhost:8100/',
+    ''
+  ];
 
+// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+const corsOptions = {
+origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+    callback(null, true);
+    } else {
+    callback(new Error('Origin not allowed by CORS'));
+    }
+}
+}
+
+// Enable preflight requests for all routes
+app.options('', cors(corsOptions));
+
+app.use(cors());
 app.use(route);
 
-app.listen(3000,()=>
+app.listen(process.env.PORT  || 3000,()=>
     console.log("Start servidor")
 )
