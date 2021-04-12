e64d8dcc2b5d68dd7a2eb75ce94c1610657e6643
Irai_Sanchez <irai.roman@fatecitapetininga.edu.br>
2021-04-12 17:07:24 -0300

    versao 12-04 11

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const route = require('./route');
 const cors = require('cors');
 const app = express();
 
-app.use(express.json());
-
 const allowedOrigins = [
     'capacitor://localhost',
     'ionic://localhost',
@@ -13,6 +11,10 @@ const allowedOrigins = [
     'http://localhost:8100/',
     ''
   ];
+
+  
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
 const corsOptions = {
@@ -29,9 +31,6 @@ origin: (origin, callback) => {
 app.options('', cors(corsOptions));
 
 app.use(cors());
-
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
 
 app.use(route);
 
