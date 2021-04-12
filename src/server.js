2e72b0887b06a0d763c65972ae71a5f64365ba61
Irai_Sanchez <irai.roman@fatecitapetininga.edu.br>
2021-04-12 14:58:38 -0300

    versao 12-04 7

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const route = require('./route');
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 const allowedOrigins = [
     'capacitor://localhost',
     'ionic://localhost',
