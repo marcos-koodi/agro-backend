2a938dcbc928dc54649d3a4d4e18b9d49196122d
Irai_Sanchez <irai.roman@fatecitapetininga.edu.br>
2021-04-12 15:18:56 -0300

    versao 12-04 10

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const route = require('./route');
 const cors = require('cors');
 const app = express();
@@ -31,8 +30,8 @@ app.options('', cors(corsOptions));
 
 app.use(cors());
 
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use(route);
 
