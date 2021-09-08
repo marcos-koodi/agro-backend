dcab4c21c6d4eac7269051fbb097856fe2b24edb
Marcos Koodi Orita <77676563+marcos-koodi@users.noreply.github.com>
2021-09-08 17:01:30 -0300

    teste

--- src/server.js
+++ src/server.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({limit: '50mb'}));
 // }
 
 // Enable preflight requests for all routes
-app.options('', cors(corsOptions));
+// app.options('', cors(corsOptions));
 
 app.use(cors());
 app.use(route);
