6bb0f9bce17e82c1a23be90e6a811be0a2157979
Marcos Koodi Orita <77676563+marcos-koodi@users.noreply.github.com>
2021-11-04 13:38:25 -0300

    alter database

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,14 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : '45.89.204.6',
-      user : 'u899272621_agro_homolog',
-      password : 'Agro@2021',
-      database : 'u899272621_agro_homolog'
-      // host : '45.89.205.188',
-      // user : 'u386024659_agroempreender',
-      // password : 'Agro@2021',
-      // database : 'u386024659_agroempreender'
+      //host : '45.89.204.6',
+      //user : 'u899272621_agro_homolog',
+      //password : 'Agro@2021',
+      //database : 'u899272621_agro_homolog'
+       host : '45.89.205.188',
+       user : 'u386024659_agroempreender',
+       password : 'Agro@2021',
+       database : 'u386024659_agroempreender'
     }
   });
 
